73302ce5ab73a50934de113d55cce6110b3a80c4
khum <khum.pun@bao-solutions.com>
2020-05-22 18:36:26 +0200

    index page added

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -2,9 +2,11 @@ from api_project.api import router
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name = 'home'),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
