3061594d6754571f0494fc90b365607945ce43f9
khum <khum.pun@bao-solutions.com>
2020-05-22 14:01:31 +0200

    all hosts allowed and api root chages

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
